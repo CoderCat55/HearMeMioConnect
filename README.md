ef5dfc9becf901793595389c1e781296b5dacf0a
CoderCat55 <121342798+CoderCat55@users.noreply.github.com>
2025-12-15 11:52:22 +0300

    öğle arası öncesi

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Readmeeee
+****** This part is for reference only 
 Okay as you can see we have a c++ code and a python code
 c++ code is just for reference it used to be my old code. Now we are improving it
 We got raspberyy pi 5 instead of esp32 and myo armbands (2) instead of other sensors.
@@ -7,19 +8,19 @@ also we will be recognizing dynamic gestures not just statics so we need some ki
 This is a real time system. 
 
 
-
 main.py the file user will run also where user will give commands frequently
 it initializes processes. getdata process and main.py is always running
 sckit-learn would do classificaiton,training etc. when triggered
 I need to have a class for sckitlearn which has methods like classify,train then call them inside main .py
 Also I need to save data (calibration) for gestures for 3 seconds. Since I will be doing dynamic and static gestures we need a time series or some time related things in order to use.
 Also we need to get data not using connection id we need to get data with device name
+*************************
 
+My aim:
+I have a webserver.py
+in main.py it should first initialize server process and server process will be going on until ctrl c
+when connect endpoint is accessed we should initialiize data aqusiiton process
 
-    calibrate
-        save data as a numpy array for sckitlearn 
-        get the gesturename  from user 
-            triggers realtime data getting using mioconnect for a specified time then saves data to shared memory
 
 Please really read all the code. 
 Do not make assumptions while answering.
--- webserver.py
+++ webserver.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, request
 import threading
 import time
 import math
+from main import *
 
 app = Flask(__name__)
 
@@ -18,40 +19,10 @@ PORT = 5002
 
 # Sensor data structure (will be populated from your shared memory)
 sensor_data = {
-    'ituemg0': 0,
-    'ituemg1': 0,
-    'ituemg2': 0,
-    'ituemg3': 0,
-    'ituemg4': 0,
-    'ituemg5': 0,
-    'ituemg6': 0,
-    'ituemg7': 0,
-     'ituax': 0,
-    'ituay': 0,
-    'ituaz': 0,
-    'itugx': 0,
-    'itugy': 0,
-    'itugz': 0,
-    'ituroll': 0,
-    'ituyaw': 0,
-    'itupitch': 0,
-    'maremg0': 0,
-    'maremg1': 0,
-    'maremg2': 0,
-    'maremg3': 0,
-    'maremg4': 0,
-    'maremg5': 0,
-    'maremg6': 0,
-    'maremg7': 0,
-    'marax': 0,
-    'maray': 0,
-    'maraz': 0,
-    'margx': 0,
-    'margy': 0,
-    'margz': 0,
-    'marroll': 0,
-    'maryaw': 0,
-    'marpitch': 0,
+    'ituemg0': 0, 'ituemg1': 0,'ituemg2': 0,'ituemg3': 0,'ituemg4': 0,'ituemg5': 0,'ituemg6': 0,'ituemg7': 0,
+    'ituax': 0,'ituay': 0,'ituaz': 0,'itugx': 0,'itugy': 0,'itugz': 0,'ituroll': 0,'ituyaw': 0,'itupitch': 0,
+    'maremg0': 0,'maremg1': 0,'maremg2': 0,'maremg3': 0,'maremg4': 0,'maremg5': 0,'maremg6': 0,'maremg7': 0,
+    'marax': 0,'maray': 0,'maraz': 0,'margx': 0,'margy': 0,'margz': 0,'marroll': 0,'maryaw': 0,'marpitch': 0,
     'cr': 0,      # Classification result
     'calword': 0,
     'connections': 0
@@ -59,11 +30,6 @@ sensor_data = {
 
 def clear_calibration_data(word_index):
     """Clear calibration data for a word"""
-    if word_index < 0 or word_index >= NUM_WORDS:
-        return False
-    
-    calibration_index[word_index] = 0
-    print(f"Cleared all data for word {word_index + 1}")
     return True
 
 
@@ -75,52 +41,35 @@ def index():
     sensor_data['connections'] += 1
     return jsonify({"status": "ok", "message": "Raspberry Pi Gesture Recognition Server"})
 
-@app.route('/')
-def index():
-    """"""
-    sensor_data['connections'] += 1
-    return jsonify({"status": "ok", "message": "Raspberry Pi Gesture Recognition Server"})
-
-
 @app.route('/data')
-def get_data():
-    """Get current sensor data"""
+def data():
+    """Get current sensor data from shared memory and update """
     return jsonify(sensor_data)
 
 
 @app.route('/classify')
 def classify():
     """Perform gesture classification"""
+    Classify(stream_buffer, stream_index, classifier)
     
     #return jsonify(sensor_data)
 
 
 @app.route('/setcw')
-def set_calibration_word():
+def setcw():
     """Save calibration sample for a word"""
     value = request.args.get('value', type=int)
+    Calibrate(value, calib_buffer, calib_index, recording_flag, recording_gesture, classifier)
+
+@app.route('/train')
+def train():
+    """Train the model"""
+    Train(classifier)
+
+@app.route('/connect')
+def connect():
+    """Data aqusition process should start when this is fetched"""
     
-
-# ==================== Helper Functions ====================
-
-def update_sensor_data_from_shared_memory():
-    """
-    PLACEHOLDER: Implement this function to read from your shared memory
-     """
-    print("you called update snespr data from shared memory")
-    pass
-
-
-def sensor_update_loop():
-    """Background thread to continuously update sensor data"""
-    while True:
-        try:
-            update_sensor_data_from_shared_memory()
-            time.sleep(0.01)  # 100Hz update rate
-        except Exception as e:
-            print(f"Sensor update error: {e}")
-            time.sleep(0.1)
-
 
 # ==================== Main ====================
 
