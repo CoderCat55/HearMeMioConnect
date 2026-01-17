6c676de3cc9301f0a1202495ae476ac59d7c4ff9
YagmurOzgul <yagmurozgulmye@gmail.com>
2026-01-17 19:14:01 +0300

    new cb files

--- main.py
+++ main.py
@@ -436,7 +436,7 @@ def TrainPersonal():
     import os
     
     print("\n=== Training Personal Model (ptr) ===")
-    user_folder = 'user'
+    user_folder = 'regennewcb'
     
     if not os.path.exists(user_folder):
         print(f"❌ Error: '{user_folder}' folder not found. Please calibrate first (cb).")
