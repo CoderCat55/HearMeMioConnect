367c2ae7973619b6104cf4d7efa85cb1320090da
YagmurOzgul <yagmurozgulmye@gmail.com>
2026-01-18 12:29:30 +0300

    new files to 10 sample (100 or 50 window sample?)

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import threading
 SAMPLINGHZ= 50
 # Calculate window sizes
 rest_window_samples = 20  #note if you are gonna change this change it also in classify function
-gesture_window_samples = 200
+gesture_window_samples = 100
 
 STREAM_BUFFER_SIZE = 1000  # ~5 seconds at SAMPLINGHZ
 CALIBRATION_BUFFER_SIZE = 600  # ~3 seconds at SAMPLINGHZ
